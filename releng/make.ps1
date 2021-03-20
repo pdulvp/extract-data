4fa9b5e534ecc0c84ab3df7a43437af63d286d4e
pdulvp <pdulvp@laposte.net>
2021-03-20 13:17:36 +0100

    [releng] Add a fixed id on debug build
    Signed-off-by: pdulvp <pdulvp@laposte.net>

--- releng/make.ps1
+++ releng/make.ps1
@@ -3,10 +3,17 @@
 $foo = (Get-Content 'manifest.json' | Out-String | ConvertFrom-Json)
 
 Write-Host -ForegroundColor Cyan "Creation of" $foo.version
-$name = $foo.version+".zip"
-if (!(Test-Path $name -PathType leaf))
-{
+$name = $foo.version+"-"+$args[0]+".zip"
+
+if ( (Test-Path $name -PathType leaf) -And ($args[0] -eq "release-unsigned")) {
+    Write-Host -ForegroundColor Red "[ERROR]" $name already exists
+
+} elseif ( (Test-Path $name -PathType leaf) -And ($args[0] -eq "debug")) {
     & "C:\Program Files\7-Zip\7z.exe" -tzip a $name manifest.json background.js _locales content_scripts icons ui 
+    Write-Host -ForegroundColor Yellow "[WARN]" $name already exists
+    
 } else {
-    Write-Host -ForegroundColor Red "[ERROR]" $foo.version exists
+    & "C:\Program Files\7-Zip\7z.exe" -tzip a $name manifest.json background.js _locales content_scripts icons ui 
+    Write-Host -ForegroundColor Green "[OK]" $name created
+    
 }
---
+++ releng/set-release.js
@@ -0,0 +1,7 @@
+﻿const fsh = require('@pdulvp/fsh');
+
+return fsh.read("manifest.json").then(p => {
+	let res = JSON.parse(p);
+	delete res.browser_specific_settings;
+	return fsh.writeIfChange("manifest.json", JSON.stringify(res, null, 2));
+});
